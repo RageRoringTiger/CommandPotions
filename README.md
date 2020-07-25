aa00399e04b29c8603c29d6a00fa3e7a72c83d88
RageRoringTiger <41753994+RageRoringTiger@users.noreply.github.com>
2020-07-25 17:19:14 -0500

    Looks

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CommandPotions
-A plugin that let's you create command potions in minecraft with the spigot api.
+**Created by RageRoringTiger**
+
+
+## Main Info
+This plugin let's you create potions that when they are drank by a player, run command(s). Then after a certain amount of time, the potion will run out and more command(s) will be run. During the time the potion is active specified particles will spawn around the player. This is all done through the config.
+
+## Commands
+There are only 2 commands you need to know:
+* /cpot <potion-name> <player> 
+  * If <player> is not set, then the potion will be given to the command sender (player who typed the command).
+* /cpot reload
+  * Reload the plugin config.
+
+## Configuration
+All of the plugin configuration is done through a simple config:
+![Config Image](/images/config.png/)
+Format: ![Alt Text](url)
+
+## Permissions
+To spawn in a potion a player need the permission *commandpotions.createpotion*. **Any** player can drink a potion to run commands.
--- src/main/java/me/rageroringtiger/commandpotion/commands/CreatePotion.java
+++ src/main/java/me/rageroringtiger/commandpotion/commands/CreatePotion.java
@@ -20,68 +20,70 @@ public class CreatePotion implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player){
-            if (args.length >= 1) {
-                Player p = (Player) sender;
-                potionMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("potions." + args[0] + ".display-name")));
-                if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("aqua")){
-                    potionMeta.setColor(Color.AQUA);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("black")){
-                    potionMeta.setColor(Color.BLACK);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("blue")){
-                    potionMeta.setColor(Color.BLUE);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("fuchsia")){
-                    potionMeta.setColor(Color.FUCHSIA);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("gray")){
-                    potionMeta.setColor(Color.GRAY);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("green")){
-                    potionMeta.setColor(Color.GREEN);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("lime")){
-                    potionMeta.setColor(Color.LIME);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("maroon")){
-                    potionMeta.setColor(Color.MAROON);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("navy")){
-                    potionMeta.setColor(Color.NAVY);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("olive")){
-                    potionMeta.setColor(Color.OLIVE);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("orange")){
-                    potionMeta.setColor(Color.ORANGE);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("purple")){
-                    potionMeta.setColor(Color.PURPLE);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("red")){
-                    potionMeta.setColor(Color.RED);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("silver")){
-                    potionMeta.setColor(Color.SILVER);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("teal")){
-                    potionMeta.setColor(Color.TEAL);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("white")){
-                    potionMeta.setColor(Color.WHITE);
-                } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("yellow")){
-                    potionMeta.setColor(Color.YELLOW);
-                } else {
-                    potionMeta.setColor(Color.BLUE);
-                }
-                if (plugin.getConfig().getInt("potions." + args[0] + ".custom-model-data") > -1){
-                    potionMeta.setCustomModelData(plugin.getConfig().getInt("potions." + args[0] + ".custom-model-data"));
-                }
-                if (plugin.getConfig().getBoolean("main-config.show-time-left")){
-                    potionMeta.addCustomEffect(new PotionEffect(PotionEffectType.LUCK, plugin.getConfig().getInt("potions." + args[0] + ".time"), 0, plugin.getConfig().getBoolean("main-config.ambient")), true);
-                }
-                if (args.length > 1) {
-                    Player target = Bukkit.getPlayerExact(args[1]);
-                    if (target instanceof Player) {
-                        target.getInventory().addItem(potion);
-                        target.sendMessage(ChatColor.GREEN + "You were given a command potion.");
-                        p.sendMessage(ChatColor.GREEN + target.getDisplayName() + " was given a command potion.");
+            if (sender.hasPermission("commandpotions.createpotion")) {
+                if (args.length >= 1) {
+                    Player p = (Player) sender;
+                    potionMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("potions." + args[0] + ".display-name")));
+                    if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("aqua")) {
+                        potionMeta.setColor(Color.AQUA);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("black")) {
+                        potionMeta.setColor(Color.BLACK);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("blue")) {
+                        potionMeta.setColor(Color.BLUE);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("fuchsia")) {
+                        potionMeta.setColor(Color.FUCHSIA);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("gray")) {
+                        potionMeta.setColor(Color.GRAY);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("green")) {
+                        potionMeta.setColor(Color.GREEN);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("lime")) {
+                        potionMeta.setColor(Color.LIME);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("maroon")) {
+                        potionMeta.setColor(Color.MAROON);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("navy")) {
+                        potionMeta.setColor(Color.NAVY);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("olive")) {
+                        potionMeta.setColor(Color.OLIVE);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("orange")) {
+                        potionMeta.setColor(Color.ORANGE);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("purple")) {
+                        potionMeta.setColor(Color.PURPLE);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("red")) {
+                        potionMeta.setColor(Color.RED);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("silver")) {
+                        potionMeta.setColor(Color.SILVER);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("teal")) {
+                        potionMeta.setColor(Color.TEAL);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("white")) {
+                        potionMeta.setColor(Color.WHITE);
+                    } else if (plugin.getConfig().getString("potions." + args[0] + ".color").equalsIgnoreCase("yellow")) {
+                        potionMeta.setColor(Color.YELLOW);
                     } else {
-                        sender.sendMessage(ChatColor.RED + "That player is not online.");
+                        potionMeta.setColor(Color.BLUE);
+                    }
+                    if (plugin.getConfig().getInt("potions." + args[0] + ".custom-model-data") > -1) {
+                        potionMeta.setCustomModelData(plugin.getConfig().getInt("potions." + args[0] + ".custom-model-data"));
+                    }
+                    if (plugin.getConfig().getBoolean("main-config.show-time-left")) {
+                        potionMeta.addCustomEffect(new PotionEffect(PotionEffectType.LUCK, plugin.getConfig().getInt("potions." + args[0] + ".time"), 0, plugin.getConfig().getBoolean("main-config.ambient")), true);
+                    }
+                    if (args.length > 1) {
+                        Player target = Bukkit.getPlayerExact(args[1]);
+                        if (target instanceof Player) {
+                            target.getInventory().addItem(potion);
+                            target.sendMessage(ChatColor.GREEN + "You were given a command potion.");
+                            p.sendMessage(ChatColor.GREEN + target.getDisplayName() + " was given a command potion.");
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "That player is not online.");
+                        }
+                    } else {
+                        potion.setItemMeta(potionMeta);
+                        p.getInventory().addItem(potion);
+                        p.updateInventory();
                     }
                 } else {
-                    potion.setItemMeta(potionMeta);
-                    p.getInventory().addItem(potion);
-                    p.updateInventory();
+                    sender.sendMessage(ChatColor.RED + "Useage: /cpot <potion> <player>");
                 }
-            } else {
-                sender.sendMessage(ChatColor.RED + "Useage: /cpot <potion> <player>");
             }
         } else {
             sender.sendMessage(ChatColor.RED + "You must be a player.");
